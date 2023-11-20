2e15e587ce1444599cbfd7ece9ea267af52db3dc
Charlatan2k <octavianmihai.calin@gmail.com>
2023-11-20 17:50:32 +0100

    Acabar funcionalidad agregar libro, eliminar y buscar por id

---
+++ Http y Express/reto3/src/routers/user.routers.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const router = Router();
+const usersCtrl = require("../controller/user.controller.js");
+
+router.get("/", usersCtrl.getStart);
+
+router.get("/books", usersCtrl.getBooks);
+
+router.get("/books/:id", usersCtrl.getBookById);
+
+router.post("/books", usersCtrl.addBook);
+
+router.put("/books", usersCtrl.modifyBook);
+
+router.delete("/books/:id", usersCtrl.deleteBook);
+
+module.exports = router;
